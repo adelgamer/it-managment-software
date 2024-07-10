637ac08955b4fd62066c7f530739856021a373fe
Adel Amrane <adelgamer814@gmail.com>
2024-07-10 17:20:58 +0100

    Test

---
+++ index.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+
+    // The lines below solved the issue
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+//Quitting the app after all the windows are closed
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+//Opening a window if no window is open
+// app.whenReady().then(() => {
+//   createWindow();
+
+//   app.on("activate", () => {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+//   });
+// });
